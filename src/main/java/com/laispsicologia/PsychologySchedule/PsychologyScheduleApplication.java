7ecc9033b1baedd136bafe7d7e589661134d47e4
Luis Parente <lgsp25@hotmail.com>
2025-07-01 21:12:40 -0300

    Project created

---
+++ src/main/java/com/laispsicologia/PsychologySchedule/PsychologyScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.laispsicologia.PsychologySchedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychologyScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychologyScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laispsicologia/PsychologySchedule/PsychologyScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laispsicologia.PsychologySchedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychologyScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
